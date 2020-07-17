c69543ac9af9cc07289727accc81c8e3bc6cc2d7
jiangchuangshi <jcsweiyi@outlook.com>
2020-07-17 21:33:25 +0800

    add assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './style/App.css';
+import {GlobalStyle} from './style';
+import { IconStyle } from './assets/iconfont/index';
 import IndexUseHooks from './page/indexLeft';
 import IndexUseClass from './page/indexRight';
 
@@ -9,13 +11,10 @@ function App (props) {
     content: '你好'
   }
   return (
-    <div className="examples">
-      <div className="left">
-        <IndexUseHooks {...page}></IndexUseHooks>
-      </div>
-      <div className="right">
-        <IndexUseClass {...page}></IndexUseClass>
-      </div>
+    <div className="App">
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      <i className="iconfont">&#xe62b;</i>
     </div>
   );
 }
