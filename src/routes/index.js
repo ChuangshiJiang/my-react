b2f1ad71150ea1b2139033b5fa9d9461926c6453
unknown <jiangchuangshi@focuschina.com>
2020-07-21 10:10:07 +0800

    add draft file test.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Home from '../application/Home/';
 import Rank from '../application/Rank/';
 import Recommend from '../application/Recommend/';
 import Singers from '../application/Singers/';
+import Test from '../test.js';
 export default [
   {
     path: '/',
@@ -30,6 +31,10 @@ export default [
         path:'/rank',
         component:Rank
       },
+      {
+        path: '/test',
+        component: Test
+      }
     ]
   }
 ]
---
+++ src/test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Test(props) {
+  return (
+    <div>test</div>
+  );
+}
+
+export default React.memo(Test);
